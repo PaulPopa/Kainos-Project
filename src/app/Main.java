4f893ce716d05d232ab4a7a5103f694f34ff93a6
Joe Z <joez@kainos.com>
2019-08-15 13:15:07 +0100

    More user input validation

--- src/app/Main.java
+++ src/app/Main.java
@@ -44,8 +44,13 @@ public class Main {
 			
 			//Login login = new Login(mapper, sc);
 			//login.verifyLogin();
+			System.out.println("Welcome to the Diamond Database for entering a new employee\n");
 			
 			String emp_id = enterID(sc);
+			
+			String f_name = enterName(sc, "f");
+			
+			String l_name = enterName(sc, "l");
 			
 			String address = enterAddress(sc);
 			
@@ -57,10 +62,6 @@ public class Main {
 
 			double start_sal = enterStartSal(sc);
 
-			String f_name = enterName(sc, "f");
-			
-			String l_name = enterName(sc, "l");
-			
 			double salary = enterSalary(sc);
 			
 			String nin = enterNin(sc);
@@ -82,84 +83,114 @@ public class Main {
 	}
 	private static String enterNin(Scanner sc) {
 		String nin;
+		boolean con = false;
+		System.out.println("Please input your employee's National Insurance Number (can be skipped and added later\n"); 
 		do {
-			System.out.println("Please input their National Insurance Number\n"); 
+			if (con) System.out.println("Invalid input. Please input National Insurance Number again (must be correctly formated)\n"); 
 			nin = sc.nextLine(); 
+			con = true;
 		} while (!nin.matches("^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]{0,1}$"));
 		return nin;
 	}
 
 	private static double enterSalary(Scanner sc) {
 		String sal;
+		boolean con = false;
+		System.out.println("Please input your employee's current salary (can be skipped and added later)\n"); 
 		do {
-			System.out.println("Please input their current salary\n"); 
+			if (con) System.out.println("Invalid input. Please enter current salary again\n"); 
 			sal = sc.nextLine(); 
+			con = true;
 		} while (sal.matches(".*[a-zA-Z]+.*"));
 		return Double.parseDouble(sal);
 	}
 
 	private static String enterName(Scanner sc, String type) {
 		String name;
+		boolean con = false;
+		if(type == "f") 
+			System.out.println("Please input your employee's first name");
+		else System.out.println("Please input your employee's last name");
 		do {
-			if(type == "f")
-			System.out.println("Please input their first name (50 character limit)\n"); 
-			else System.out.println("Please input their last name (50 character limit)\n"); 
+			if (con && type =="f") System.out.println("Invalid input. Please enter first name again (50 character limit)");
+			else {
+				if (con) System.out.println("Invalid input. Please enter last name again (50 character limit)\n"); 
+			}
 			name = sc.nextLine(); 
-		} while (!name.matches(".*[a-zA-Z]+.*") && name.length() > 50);
+			con = true;
+		} while (name.matches(".*[0-9]+.*") && name.length() > 50);
 		return name;
 	}
 
 	private static double enterStartSal(Scanner sc) {
 		String startsal;
+		boolean con = false;
+		System.out.println("Please input your employee's initial starting salary (can be skipped and added later)\n"); 
 		do {
-			System.out.println("Please input their initial starting salary\n"); 
+			if (con) System.out.println("Invalid input. Please enter starting salary again\n"); 
 			startsal = sc.nextLine(); 
+			con = true;
 		} while (startsal.matches(".*[a-zA-Z]+.*"));
 		return Double.parseDouble(startsal);
 	}
 
 	private static String enterSortCode(Scanner sc) {
 		String sortcode;
+		boolean con = false;
+		System.out.println("Please input your employee's sort code (6 digits - can be skipped and added later)\n"); 
 		do {
-			System.out.println("Please input their sort code (6 digits)\n"); 
+			if (con) System.out.println("Invalid input. Please enter sort code again (must be 6 digits)\n"); 
 			sortcode = sc.nextLine(); 
 			sortcode = sortcode.replace("-", "");
+			con = true;
 		} while (sortcode.length() != 6 && sortcode.matches(".*[a-zA-Z]+.*"));
 		return sortcode;
 	}
 
 	private static String enterBankAcc(Scanner sc) {
 		String bankacc;
+		boolean con = false;
+		System.out.println("Please input your employee's bank account (8 digits - can be skipped and added later)\n"); 
 		do {
-			System.out.println("Please input their bank account details (8 digits) \n"); 
-			bankacc = sc.nextLine(); 
-		} while (bankacc.length() != 8 && !bankacc.contains("[a-zA-Z]+"));
+			if (con) System.out.println("Invalid input. Please enter bank account again (must be 8 digits)\n"); 
+			bankacc = sc.nextLine();
+			con = true;
+		} while ((bankacc.length() != 8 || bankacc.length() != 0) && bankacc.matches(".*[a-zA-Z]+.*"));
 		return bankacc;
 	}
-
+//
 	private static String enterEmail(Scanner sc) {
 		String email;
+		boolean con = false;
+		System.out.println("Please input your employee's email address\n"); 
 		do {
-			System.out.println("Please input their email address (must be less than 50 characters)\n"); 
+			if (con) System.out.println("Invalid input. Please enter email address again (must be less than 50 characters)\n"); 
 			email = sc.nextLine(); 
+			con = true;
 		} while (!isValid(email));
 		return email;
 	}
 
 	private static String enterAddress(Scanner sc) {
 		String addr;
+		boolean con = false;
+		System.out.println("Please input your employee's address\n"); 
 		do {
-			System.out.println("Please input their living address (must be less than 50 characters)\n"); 
+			if (con) System.out.println("Invalid input. Please enter address again (must be less than 50 characters)\n"); 
 			addr = sc.nextLine(); 
+			con = true;
 		} while (addr.length() > 50);
 		return addr;
 	}
 
 	private static String enterID(Scanner sc) {
 		String emp_id;
+		boolean con = false;
+		System.out.println("Please input your new employee's id (8 characters)\n"); 
 		do {
-			System.out.println("Please input your new employee id (must be 8 characters)\n"); 
+			if (con) System.out.println("Invalid input. Please enter employee id again (must be 8 characters)\n"); 
 			emp_id = sc.nextLine(); 
+			con = true;
 		} while (emp_id.length() != 8);
 		return emp_id;
 		
