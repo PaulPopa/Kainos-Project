fa77ee6c8ff1c3b062600bf399a3ba989de74d10
Joe Z <joez@kainos.com>
2019-08-15 11:26:14 +0100

    Create user script

--- scripts/createUser.sql
+++ scripts/createUser.sql
@@ -1,2 +1,12 @@
 CREATE USER hr@localhost IDENTIFIED WITH mysql_native_password BY 'hrpass';
-GRANT SELECT ON company.* to hr@localhost;
+GRANT SELECT, INSERT ON company.employee to hr@localhost;
+
+CREATE USER finance@localhost IDENTIFIED WITH mysql_native_password BY 'finpass';
+GRANT SELECT ON company.employee to finance@localhost;
+
+CREATE USER sales@localhost IDENTIFIED WITH mysql_native_password BY 'salespass';
+GRANT SELECT ON company.salesEmployee to sales@localhost;
+
+CREATE USER talent@localhost IDENTIFIED WITH mysql_native_password BY 'talpass';
+GRANT SELECT ON company.projects, company.assignment, (employee_id, f_name, l_name)company.employee to talent@localhost;
+
