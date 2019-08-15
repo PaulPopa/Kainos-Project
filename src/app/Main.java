ba5e88110040b66f3a7b6c3c44725cf583d946b8
Joe Z <joez@kainos.com>
2019-08-15 12:30:03 +0100

    fixed user input validation for entering new employee

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,9 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.security.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -39,42 +42,131 @@ public class Main {
 			EmployeesMapper mapper = session.getMapper(EmployeesMapper.class);
 			Scanner sc = new Scanner(System.in);
 			
-			Login login = new Login(mapper, sc);
-			login.verifyLogin();
+			//Login login = new Login(mapper, sc);
+			//login.verifyLogin();
 			
-			System.out.println("Please input your new employee id\n"); 
-			String emp_id = sc.nextLine();
-			System.out.println("Please input the address\n"); 
-			String address = sc.nextLine();
-			System.out.println("Please input the email\n"); 
-			String email = sc.nextLine();
-			System.out.println("Please input the bank account\n"); 
-			String bank_account = sc.nextLine();
-			System.out.println("Please input the sort code\n"); 
-			String sort_code = sc.nextLine();
-			System.out.println("Please input the starting salary\n"); 
-			double start_sal = Double.parseDouble(sc.nextLine());
-			System.out.println("Please input the first name\n"); 
-			String f_name = sc.nextLine();
-			System.out.println("Please input the last name\n"); 
-			String l_name = sc.nextLine();
-			System.out.println("Please input the current salary\n"); 
-			double salary = Double.parseDouble(sc.nextLine());
-			System.out.println("Please input the National Insurance Number\n"); 
-			String nin = sc.nextLine();
-			System.out.println("Is this the employee you want to insert? Yes/No \n"); 
+			String emp_id = enterID(sc);
+			
+			String address = enterAddress(sc);
+			
+			String email = enterEmail(sc);
+			
+			String bank_account = enterBankAcc(sc);
+			
+			String sort_code = enterSortCode(sc);
+
+			double start_sal = enterStartSal(sc);
+
+			String f_name = enterName(sc, "f");
+			
+			String l_name = enterName(sc, "l");
+			
+			double salary = enterSalary(sc);
+			
+			String nin = enterNin(sc);
+			
+			System.out.println("Adding this employee to database: \n"); 
+
 			System.out.println(emp_id + " " + address + " " + email + " " + bank_account + " " + sort_code + " " + start_sal + " " + f_name + " " + l_name + " " + salary + " " + nin);
-			String yes = sc.nextLine();
-			if (yes == "yes") {
+			//String yes = sc.nextLine();
+			//if (yes == "yes") {
 			Employee e = new Employee(emp_id, address, email, bank_account, sort_code, start_sal, f_name, l_name, salary, nin);
 			mapper.insertEmployee(e);
-			System.out.println(e.getF_name());
 			session.commit();
 			System.out.println("Employee added");
-			}
+			//}
 
 		} finally {
 			session.close();
 		}
 	}
+	private static String enterNin(Scanner sc) {
+		String nin;
+		do {
+			System.out.println("Please input their National Insurance Number\n"); 
+			nin = sc.nextLine(); 
+		} while (!nin.matches("^[A-CEGHJ-PR-TW-Z]{1}[A-CEGHJ-NPR-TW-Z]{1}[0-9]{6}[A-DFM]{0,1}$"));
+		return nin;
+	}
+
+	private static double enterSalary(Scanner sc) {
+		String sal;
+		do {
+			System.out.println("Please input their current salary\n"); 
+			sal = sc.nextLine(); 
+		} while (sal.matches(".*[a-zA-Z]+.*"));
+		return Double.parseDouble(sal);
+	}
+
+	private static String enterName(Scanner sc, String type) {
+		String name;
+		do {
+			if(type == "f")
+			System.out.println("Please input their first name (50 character limit)\n"); 
+			else System.out.println("Please input their last name (50 character limit)\n"); 
+			name = sc.nextLine(); 
+		} while (!name.matches(".*[a-zA-Z]+.*") && name.length() > 50);
+		return name;
+	}
+
+	private static double enterStartSal(Scanner sc) {
+		String startsal;
+		do {
+			System.out.println("Please input their initial starting salary\n"); 
+			startsal = sc.nextLine(); 
+		} while (startsal.matches(".*[a-zA-Z]+.*"));
+		return Double.parseDouble(startsal);
+	}
+
+	private static String enterSortCode(Scanner sc) {
+		String sortcode;
+		do {
+			System.out.println("Please input their sort code (6 digits)\n"); 
+			sortcode = sc.nextLine(); 
+			sortcode = sortcode.replace("-", "");
+		} while (sortcode.length() != 6 && sortcode.matches(".*[a-zA-Z]+.*"));
+		return sortcode;
+	}
+
+	private static String enterBankAcc(Scanner sc) {
+		String bankacc;
+		do {
+			System.out.println("Please input their bank account details (8 digits) \n"); 
+			bankacc = sc.nextLine(); 
+		} while (bankacc.length() != 8 && !bankacc.contains("[a-zA-Z]+"));
+		return bankacc;
+	}
+
+	private static String enterEmail(Scanner sc) {
+		String email;
+		do {
+			System.out.println("Please input their email address (must be less than 50 characters)\n"); 
+			email = sc.nextLine(); 
+		} while (!isValid(email));
+		return email;
+	}
+
+	private static String enterAddress(Scanner sc) {
+		String addr;
+		do {
+			System.out.println("Please input their living address (must be less than 50 characters)\n"); 
+			addr = sc.nextLine(); 
+		} while (addr.length() > 50);
+		return addr;
+	}
+
+	private static String enterID(Scanner sc) {
+		String emp_id;
+		do {
+			System.out.println("Please input your new employee id (must be 8 characters)\n"); 
+			emp_id = sc.nextLine(); 
+		} while (emp_id.length() != 8);
+		return emp_id;
+		
+	}
+	
+	   static boolean isValid(String email) {
+		      String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		      return email.matches(regex);
+		   }
 }
